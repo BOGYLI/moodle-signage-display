edc00542e96be9bbe65e3983e4f11329dba864f6
Andre Scherl <andre.scherl@schule.bayern.de>
2023-03-09 20:32:07 +0100

    Added timer to repeat presentation update

--- __main__.py
+++ __main__.py
@@ -1,21 +1,29 @@
 import config
+import threading
 from MoodleWebService import MoodleWebService
 from PresentationParser import PresentationParser
 from bs4 import BeautifulSoup
 
-# Get data from mebis/moodle webservice.
-mws = MoodleWebService(config.moodleurl+"/webservice/rest/server.php", config.wstoken)
-fields = mws.request_db_fields(config.databaseid)
-entries = mws.request_db_entries(config.databaseid)
-parser = PresentationParser(mws)
-slides = parser.collect_presentation_slides(entries, fields)
-slides_html = parser.build_slides_html(slides)
 
-with open("template.html", "r") as tf:
-    soup = BeautifulSoup(tf, 'html.parser')
+def perform_presentation_update():
+    threading.Timer(300, perform_presentation_update).start()
     
-slidesdiv = soup.find(attrs={"class": "slides"})
-slidesdiv.append(slides_html)
+    # Get data from mebis/moodle webservice.
+    mws = MoodleWebService(config.moodleurl+"/webservice/rest/server.php", config.wstoken)
+    fields = mws.request_db_fields(config.databaseid)
+    entries = mws.request_db_entries(config.databaseid)
+    parser = PresentationParser(mws)
+    slides = parser.collect_presentation_slides(entries, fields)
+    slides_html = parser.build_slides_html(slides)
 
-with open("html/index.html", "w", encoding="utf-8") as pf:
-    pf.write(soup.prettify(formatter=None))
+    with open("template.html", "r") as tf:
+        soup = BeautifulSoup(tf, 'html.parser')
+        
+    slidesdiv = soup.find(attrs={"class": "slides"})
+    slidesdiv.append(slides_html)
+
+    with open("html/index.html", "w", encoding="utf-8") as pf:
+        pf.write(soup.prettify(formatter=None))
+
+# run the endless loop.
+perform_presentation_update()
